4c25e5379f30a57a736cb437e417348ebb15478d
Priya Bansal <66693217+priyabnsal@users.noreply.github.com>
2021-04-16 11:34:02 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -62,7 +62,8 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <NavLink className="navbar-brand" to="#">
-                        <img src={logo} alt="logo" />
+//                         <img src={logo} alt="logo" />
+                        <h3>MERN STACK</h3>
             </NavLink>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
